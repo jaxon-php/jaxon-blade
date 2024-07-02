5f52b8fa2a0f883e381b962133eabe64448877cc
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-07-02 20:51:06 +0200

    New directives for Jaxon custom attributes.

--- src/View.php
+++ src/View.php
@@ -2,28 +2,58 @@
 
 namespace Jaxon\Blade;
 
-use Jaxon\Contracts\View as ViewContract;
-use Jaxon\Utils\View\Store;
+use Jaxon\App\View\ViewInterface;
+use Jaxon\App\View\Store;
+use Jenssegers\Blade\Blade;
 
-use Jenssegers\Blade\Blade as Renderer;
+use function preg_replace;
+use function trim;
 
-class View implements ViewContract
+class View implements ViewInterface
 {
+    /**
+     * @var Blade
+     */
+    private Blade $xRenderer;
+
+    /**
+     * The constructor
+     */
     public function __construct()
     {
-        $this->xRenderer = new Renderer(__DIR__ . '/../views', __DIR__ . '/../cache');
+        $this->xRenderer = new Blade(__DIR__ . '/../views', __DIR__ . '/../cache');
+
+        // Directives for Jaxon custom attributes
+        $this->xRenderer->directive('jxnHtml', function ($expression) {
+            return '<?php echo \Jaxon\attr()->html(' . $this->expr($expression) . '); ?>';
+        });
+        $this->xRenderer->directive('jxnShow', function ($expression) {
+            return '<?php echo \Jaxon\attr()->show(' . $this->expr($expression) . '); ?>';
+        });
+        $this->xRenderer->directive('jxnTarget', function ($expression) {
+            return '<?php echo \Jaxon\attr()->target(' . $expression . '); ?>';
+        });
+        $this->xRenderer->directive('jxnOn', function ($expression) {
+            return '<?php echo \Jaxon\attr()->on(' . $this->expr($expression) . '); ?>';
+        });
     }
 
     /**
-     * Add a namespace to this view renderer
+     * Replace Jaxon functions with their full names
      *
-     * @param string        $sNamespace         The namespace name
-     * @param string        $sDirectory         The namespace directory
-     * @param string        $sExtension         The extension to append to template names
+     * @param string $expression The directive parameter
      *
-     * @return void
+     * @return string
      */
-    public function addNamespace($sNamespace, $sDirectory, $sExtension = '')
+    private function expr(string $expression)
+    {
+        return preg_replace('/([\(\s\,])(rq|jq|js|pm)\(/', '${1}\\Jaxon\\\${2}(', $expression);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addNamespace(string $sNamespace, string $sDirectory, string $sExtension = '')
     {
         if(($sNamespace) && ($sDirectory))
         {
@@ -36,21 +66,13 @@ class View implements ViewContract
     }
 
     /**
-     * Render a view
-     *
-     * @param Store         $store        A store populated with the view data
-     *
-     * @return string        The string representation of the view
+     * @inheritDoc
      */
-    public function render(Store $store)
+    public function render(Store $store): string
     {
-        $sViewName = $store->getViewName();
         $sNamespace = $store->getNamespace();
-        // In this view renderer, the namespace must always be prepended to the view name.
-        if(substr($sViewName, 0, strlen($sNamespace) + 2) != $sNamespace . '::')
-        {
-            $sViewName = $sNamespace . '::' . $sViewName;
-        }
+        $sViewName = !$sNamespace ? $store->getViewName() :
+            $sNamespace . '::' . $store->getViewName();
 
         // Render the template
         return trim($this->xRenderer->render($sViewName, $store->getViewData()), " \t\n");
