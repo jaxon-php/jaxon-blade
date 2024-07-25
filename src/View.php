a3d834ef7bc31a4bfb5e8f7bdfef367eb4edd6cc
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-07-25 08:42:19 +0200

    New template functions.

--- src/View.php
+++ src/View.php
@@ -42,11 +42,14 @@ class View implements ViewInterface
         $this->xRenderer->directive('jxnShow', function($expression) {
             return '<?php echo \Jaxon\attr()->show(' . $this->expr($expression) . '); ?>';
         });
-        $this->xRenderer->directive('jxnTarget', function($expression) {
-            return '<?php echo \Jaxon\attr()->target(' . $expression . '); ?>';
-        });
         $this->xRenderer->directive('jxnOn', function($expression) {
             return '<?php echo \Jaxon\attr()->on(' . $this->expr($expression) . '); ?>';
+        });
+        $this->xRenderer->directive('jxnClick', function($expression) {
+            return '<?php echo \Jaxon\attr()->click(' . $this->expr($expression) . '); ?>';
+        });
+        $this->xRenderer->directive('jxnTarget', function($expression) {
+            return '<?php echo \Jaxon\attr()->target(' . $expression . '); ?>';
         });
 
         // Directives for Jaxon Js and CSS codes
